f53c96c21ae3de4ec7411e9c652cf89e59af222f
王诛魔 <wangzhumoo@gmail.com>
2019-03-23 02:01:39 +0800

    [feature]sdl render init

--- src/first_sdl.c
+++ src/first_sdl.c
@@ -3,10 +3,18 @@
 
 int main(int argc,char *argv[])
 {
+
+  //window
   SDL_Window *window = NULL; 
+  //render
+  SDL_Renderer *render = NULL;
 
-  SDL_Init(SDL_INIT_VIDEO);
+  //init all sdl function
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+  SDL_Log("SDL Init.");
 
+  //***Create Window
+  //window create
   window = SDL_CreateWindow("SDL2 Window",
       200,
       200,
@@ -15,12 +23,30 @@ int main(int argc,char *argv[])
       SDL_WINDOW_SHOWN);
 
   //window nonull 
-  if(!window)
-  {
-    printf("Failed to Create window!");
+  if(!window){
+    SDL_Log("Failed to Create window!");
     goto _EXIT;
   }
-  
+
+  //***Create Render
+  render = SDL_CreateRenderer(window, 0, 0);
+  if (!render) {
+    SDL_Log("Failed to Create Render!");
+    goto _EXIT_WINDOW;
+  }
+  SDL_Log("Init Render.");
+
+  SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
+
+  //clear this render
+  SDL_RenderClear(render);
+
+  //set render to window
+  SDL_RenderPresent(render);
+
+  SDL_Delay(6000);
+
+_EXIT_WINDOW:
   SDL_DestroyWindow(window);
   
 _EXIT:
