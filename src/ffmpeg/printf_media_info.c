297cf3428aabbc1f91cf5a7edf067573f2235a35
wangzhumo <wangzhumoo@gmail.com>
2019-03-28 01:23:40 +0800

    [feature]添加了media_info

---
+++ src/ffmpeg/printf_media_info.c
@@ -0,0 +1,7 @@
+//
+// Created by 王诛魔 on 2019/3/28.
+//
+
+#include "printf_media_info.h"
+
+
---
+++ src/ffmpeg/printf_media_info.h
@@ -0,0 +1,12 @@
+//
+// Created by 王诛魔 on 2019/3/28.
+//
+
+#ifndef STREAMING_PRINTF_MEDIA_INFO_H
+#define STREAMING_PRINTF_MEDIA_INFO_H
+
+#endif //STREAMING_PRINTF_MEDIA_INFO_H
+
+#include <libavutil/avutil.h>
+#include <libavformat/avformat.h>
+
