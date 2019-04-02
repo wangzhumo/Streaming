ad300327077fdb4d4b3dde43280df0b70be852fd
wangzhumo <wangzhumoo@gmail.com>
2019-04-03 01:54:20 +0800

    [feature]明天搞定这个问题

---
+++ src/sdl/surface_sdl.h
@@ -0,0 +1,13 @@
+//
+// Created by 王诛魔 on 2019/4/3.
+//
+
+#ifndef STREAMING_SURFACE_SDL_H
+#define STREAMING_SURFACE_SDL_H
+
+#endif //STREAMING_SURFACE_SDL_H
+
+#include <SDL.h>
+#include <stdio.h>
+
+void show_with_surface();
